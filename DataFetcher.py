697b503fb4d365156253ac1ea1bbad673c10dadf
Tuomas Mäenpää <tuomas.maenpaa@tuni.fi>
2022-03-16 12:34:28 +0200

    Added a dummy JSON from STATFI
    dummy JSON added for development purposes. Initialized Datafetcher.

---
+++ DataFetcher.py
@@ -0,0 +1,19 @@
+
+class DataFetcher():
+    def __init__(self) -> None:
+        pass
+
+    
+    def get_historical():
+        return 1
+
+    def get_realtime():
+        return 1
+    
+    def get_stations():
+        return 1
+
+    
+ 
+    
+
